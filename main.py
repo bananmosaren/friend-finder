a2b0e6316fdfbf17adfac94603d7afee00d51aab
bananmosaren <fred.josefsson@gmail.com>
2025-06-04 00:15:03 +0200

    livestream, hopefully

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import flask
-from flask import Flask, render_template, Response, request, jsonify
-from aiortc import RTCPeerConnection, RTCSessionDescription
-import cv2
-import json
-import uuid
-import asyncio
-import logging
-import time
 
 app = flask.Flask(__name__)
 
@@ -16,57 +8,18 @@ def home():
     return flask.render_template("index.html", ip=ip)
     
 def generate_frames():
-    camera = cv2.VideoCapture(0) # ==============IMPORTANT============
+    camera = cv2.VideoCapture(0)  # Use 0 for the default camera
     while True:
-        start_time = time.time()
-        success, frame = camera.read()
+        success, frame = camera.read()  # Read the camera frame
         if not success:
             break
         else:
+            # Encode the frame in JPEG format
             ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            # concat frame one by one and show result
+            frame = buffer.tobytes()  # Convert to bytes
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
-            elapsed_time = time.time() - start_time
-            logging.debug(f"Frame generation time: {elapsed_time} seconds")
-                
-@app.route('/user')
-def user():
-    return render_template('user.html')
-    
-async def offer_async():
-    params = await request.json
-    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # Yield the frame
 
-    # Create an RTCPeerConnection instance
-    pc = RTCPeerConnection()
-
-    # Generate a unique ID for the RTCPeerConnection
-    pc_id = "PeerConnection(%s)" % uuid.uuid4()
-    pc_id = pc_id[:8]
-
-    # Create and set the local description
-    await pc.createOffer(offer)
-    await pc.setLocalDescription(offer)
-
-    # Prepare the response data with local SDP and type
-    response_data = {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-
-    return jsonify(response_data)
-    
-def offer():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    
-    future = asyncio.run_coroutine_threadsafe(offer_async(), loop)
-    return future.result()
-    
-# Route to handle the offer request
-@app.route('/offer', methods=['POST'])
-def offer_route():
-    return offer()
-    
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
