7cb1e5f38e0ced1147de0df0f8f7851da48f83b1
bananmosaren <fred.josefsson@gmail.com>
2025-06-05 00:26:33 +0200

    locations...

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -58,3 +58,28 @@ navigator.geolocation.getCurrentPosition(
 		console.error("Error getting location: ", error);
 	}
 );
+
+function getLocations() {
+	fetch('/current_locations')
+		.then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok: ' + response.statusText);
+		}
+			return response.json();
+		})
+		.then(data => {
+			locations = data;
+			console.log(data);
+			
+			locations.forEach(item => {
+				console.log(`user_id: ${item.id}, lat: ${item.latitude}, lng: ${item.longitude}`);
+				const marker = new maplibregl.Marker()
+					.setLngLat([item.longitude, item.latitude])
+					.addTo(map);
+					console.log("new marker")
+			})
+		})
+		.catch(error => console.error('Error fetching: ', error));
+}
+
+getLocations();
