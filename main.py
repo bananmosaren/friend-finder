a2ecb1ff575a1c8a509750d23a033765a2b737ee
bananmosaren <fred.josefsson@gmail.com>
2025-06-04 15:43:03 +0200

    ip

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
-    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    ip = requests.headers.get('X-Forwarded-For', request.remote_addr)
     return flask.render_template("index.html", ip=ip)
     
 if __name__ == "__main__":
